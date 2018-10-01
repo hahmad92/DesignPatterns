3824a5daaa2cd18fb1d2b1447a93840cacae3c19
Hammad Ahmad <hammadawan50@yahoo.com>
2018-10-01 10:00:01 +0500

    First Commit

---
+++ src/facade/coursera/IAccount.java
@@ -0,0 +1,13 @@
+package facade.coursera;
+
+import java.math.BigDecimal;
+
+public interface IAccount {
+	public void deposit(BigDecimal amount);
+
+	public void withdraw(BigDecimal amount);
+
+	public void transfer(IAccount account, BigDecimal amount);
+
+	public int getAccountNumber();
+}
