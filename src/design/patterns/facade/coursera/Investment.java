75ca48305c2595a136e66be8a37b145793c56365
hahmad01 <hammadawan50@yahoo.com>
2018-10-02 20:48:32 +0500

    Facade Pattern added

---
+++ src/design/patterns/facade/coursera/Investment.java
@@ -0,0 +1,35 @@
+package design.patterns.facade.coursera;
+
+import java.math.BigDecimal;
+
+public class Investment implements IAccount {
+
+	public Investment(BigDecimal initAmount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void deposit(BigDecimal amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void withdraw(BigDecimal amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void transfer(IAccount account, BigDecimal amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getAccountNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/design/patterns/facade/coursera/Saving.java
@@ -0,0 +1,35 @@
+package design.patterns.facade.coursera;
+
+import java.math.BigDecimal;
+
+public class Saving implements IAccount {
+
+	public Saving(BigDecimal initAmount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void deposit(BigDecimal amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void withdraw(BigDecimal amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void transfer(IAccount account, BigDecimal amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getAccountNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
