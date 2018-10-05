d7584f3067b281b9c5af6e436ed89b7186a2e8c4
hahmad01 <hammadawan50@yahoo.com>
2018-10-05 21:53:01 +0500

    Observer Pattern from Head first is committed

---
+++ src/design/patterns/observer/headfirst/example1/DisplayElement.java
@@ -0,0 +1,5 @@
+package design.patterns.observer.headfirst.example1;
+
+public interface DisplayElement {
+	public void display();
+}
---
+++ src/design/patterns/observer/headfirst/example1/Observer.java
@@ -0,0 +1,5 @@
+package design.patterns.observer.headfirst.example1;
+
+public interface Observer {
+	public void update(float temp, float humidity, float pressure);
+}
